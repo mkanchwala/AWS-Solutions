abe3c532586a39c5d1f7a06962bd2156043d51fb
mkanchwala <murtuza.rajkahn@gmail.com>
2015-06-26 12:00:16 +0530

    Initial Commit

---
+++ spring-s3-upload/src/main/java/com/mkanchwala/s3/Application.java
@@ -0,0 +1,16 @@
+package com.mkanchwala.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mkanchwala
+ *http://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.definition
+ *http://www.petrikainulainen.net/programming/spring-framework/spring-data-jpa-tutorial-part-two-crud/
+ */
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
